5d9d85b60dc154bb11b5daf2e40debd5f4bc3f4c
Steven Velozo <steven@velozo.com>
2024-02-26 16:08:49 -0800

    New version of cli utility library for better command classes

--- debug/Harness.js
+++ debug/Harness.js
@@ -2,8 +2,8 @@ console.log('Fancy debug harness here.  Shame if it would get quacked.');
 
 let libQuackage = require('../source/Quackage-CLIProgram.js');
 
+//libQuackage.run(['node', 'Harness.js', 'bp', 'pictunittest']);
+libQuackage.run(['node', 'Harness.js', 'lint']);
+
 //libQuackage.run(['node', 'Harness.js', 'db', 'dist/documentation', '--meadow', 'model/MeadowModel-Extended.json']);
-
-libQuackage.run(['node', 'Harness.js', 'db', 'dist/documentation', '--meadow', 'model/MeadowModel-Extended.json', '-d', 'documentation-definition.json']);
-
-//libQuackage.run(['node', 'Harness.js', 'db', 'dist/documentation', '--meadow', 'lib/headlight_model/json/Headlight-Extended.json', '-d', 'lib/headlight_model/docs/customer-documentation-definition.json']);
+//libQuackage.run(['node', 'Harness.js', 'db', 'dist/documentation', '--meadow', 'model/MeadowModel-Extended.json', '-d', 'documentation-definition.json']);
