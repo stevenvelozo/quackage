989e497c5173f3c10ca5344012a7d34b42966882
Steven Velozo <steven@velozo.com>
2025-03-06 08:13:33 -0800

    Harness changes

--- debug/Harness.js
+++ debug/Harness.js
@@ -5,7 +5,7 @@ let libQuackage = require('../source/Quackage-CLIProgram.js');
 //libQuackage.run(['node', 'Harness.js', 'bp', 'pictunittest']);
 //libQuackage.run(['node', 'Harness.js', 'lint']);
 
-//libQuackage.run(['node', 'Harness.js', 'csvcheck', 'testdata/airports.csv', '-c']);
+libQuackage.run(['node', 'Harness.js', 'csvcheck', 'testdata/airports.csv', '-c']);
 //libQuackage.run(['node', 'Harness.js', 'csvtransform', 'testdata/airports.csv', '-x']);
 //libQuackage.run(['node', 'Harness.js', 'csvtransform', 'testdata/airports.csv', '-x', '-m', 'testdata/Airport-Entity-Mapping.json']);
 //libQuackage.run(['node', 'Harness.js', 'csvtransform', 'testdata/airports.csv']);
@@ -13,7 +13,7 @@ let libQuackage = require('../source/Quackage-CLIProgram.js');
 
 //libQuackage.run(['node', 'Harness.js', 'csvintersect', 'testdata/Fruit-Color.csv', '-f', 'Fruit', '-s', 'Color', '-x']);
 //libQuackage.run(['node', 'Harness.js', 'csvintersect', 'testdata/Fruit-Color.csv', '-f', 'Fruit', '-s', 'Color']);
-libQuackage.run(['node', 'Harness.js', 'csvintersect', 'testdata/Fruit-Color.csv', '-f', 'Fruit', '-s', 'Color', '-o', 'dist/Fruit-Color.json']);
+//libQuackage.run(['node', 'Harness.js', 'csvintersect', 'testdata/Fruit-Color.csv', '-f', 'Fruit', '-s', 'Color', '-o', 'dist/Fruit-Color.json']);
 
 //libQuackage.run(['node', 'Harness.js', 'ajv', 'html/', '-p', 'PictoView']);
 
